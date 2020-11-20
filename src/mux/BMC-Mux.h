41eebaa574614e6a47e9ae02c8fc65362a7661e5
Nero <neroroxxx@gmail.com>
2020-11-20 09:54:57 -0500

    v1.0.2 fixed version not updating

--- src/mux/BMC-Mux.h
+++ src/mux/BMC-Mux.h
@@ -28,19 +28,24 @@
 #endif
 
 class BMCMux {
+
 private:
 #if BMC_MAX_MUX_GPIO > 0
   BMCMuxGpio muxGpio;
 #endif
+
 #if BMC_MAX_MUX_IN > 0
   BMCMuxIn muxIn;
 #endif
+
 #if BMC_MAX_MUX_OUT > 0
   BMCMuxOut muxOut;
 #endif
+
 #if BMC_MAX_MUX_IN_ANALOG > 0
   BMCMuxInAnalog muxInAnalog;
 #endif
+
 public:
   BMCMux(){
 
@@ -78,6 +83,8 @@ public:
   // digitalRead equivalent
   bool readDigital(uint8_t n){
 #if BMC_MAX_MUX_GPIO > 0
+    // if MUX GPIO is available we'll check it first
+    // muxGpio.readPin() will check to see if this pin is assigned to this mux
     if(n < BMC_MAX_MUX_GPIO){
       return muxGpio.readPin(n);
     }
@@ -102,44 +109,16 @@ public:
     muxOut.setPinValue(n, on);
 #endif
   }
-
-
-
-
-
-  // when using custom MUX
-  void setDigitalValue(uint8_t t_pin, bool t_value){
-#if BMC_MAX_MUX_GPIO > 0
-    if(t_pin < BMC_MAX_MUX_GPIO){
-      muxGpio.setPinValue(t_pin, t_value);
-      return;
-    }
-#endif
-
-#if BMC_MAX_MUX_IN > 0
-    muxIn.setPinValues(t_pin, t_value);
-#endif
-  }
-
-  
-  bool getDigitalValue(uint8_t t_pin){
-#if BMC_MAX_MUX_GPIO > 0
-    if(t_pin < BMC_MAX_MUX_GPIO){
-      return muxGpio.getPinValue(t_pin);
-    }
-#endif
-
-#if BMC_MAX_MUX_OUT > 0
-    return muxOut.getPinValue(t_pin);
-#endif
+  // read an analog mux input pin
+  uint16_t readAnalog(uint8_t n){
+#if BMC_MAX_MUX_IN_ANALOG > 0
+    return muxInAnalog.getPinValue(n);
+#else
     return 0;
+#endif
   }
 
-
-
-
-
-
+  // testing for LEDs
   void testDigital(uint8_t n){
 #if BMC_MAX_MUX_GPIO > 0
     if(n < BMC_MAX_MUX_GPIO){
@@ -153,15 +132,43 @@ public:
 #endif
   }
 
-  // read an analog mux input pin
-  uint16_t readAnalog(uint8_t n){
-#if BMC_MAX_MUX_IN_ANALOG > 0
-    return muxInAnalog.getPinValue(n);
-#else
-    return 0;
+  // when using custom MUX
+  // set their value via the API
+  // you would read a non-supported chip and pass this function it's state
+  // then BMC will give this value to buttons and encoders via readDigital()
+  void setDigitalValue(uint8_t t_pin, bool t_value){
+#if BMC_MAX_MUX_GPIO > 0
+    if(t_pin < BMC_MAX_MUX_GPIO){
+      muxGpio.setPinValue(t_pin, t_value);
+      return;
+    }
+#endif
+
+#if BMC_MAX_MUX_IN > 0
+    muxIn.setPinValues(t_pin, t_value);
 #endif
   }
-  // read an analog mux input pin
+
+  // when using custom MUX
+  // get their state via the API for digital outputs
+  // when leds are updated, their state is passed BMCMux
+  // then the value is stored in a variable, this states are returned by this
+  // functions, in your sketch you would program your non-supported chip
+  // then get the state of each of these pins and pass them to your chip
+  bool getDigitalValue(uint8_t t_pin){
+#if BMC_MAX_MUX_GPIO > 0
+    if(t_pin < BMC_MAX_MUX_GPIO){
+      return muxGpio.getPinValue(t_pin);
+    }
+#endif
+
+#if BMC_MAX_MUX_OUT > 0
+    return muxOut.getPinValue(t_pin);
+#endif
+    return 0;
+  }
+
+  // same as setDigitalValue but instead if takes in a 10-bit value (0 to 1023)
   void setAnalogValue(uint8_t n, uint16_t t_value){
 #if BMC_MAX_MUX_IN_ANALOG > 0
     muxInAnalog.setPinValue(n, t_value);
