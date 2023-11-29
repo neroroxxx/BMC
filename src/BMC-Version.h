195f25410bf9446720c5e7f3068eed49ab476ad8
Nero <neroroxxx@gmail.com>
2023-11-28 21:14:24 -0500

    v2.2.1 fixes bug with ili displays

--- src/BMC-Version.h
+++ src/BMC-Version.h
@@ -23,9 +23,9 @@
 // BMC Version stored in EEPROM (for editor usage)
 #define BMC_VERSION_MAJ     2
 #define BMC_VERSION_MIN     2
-#define BMC_VERSION_PATCH   0
+#define BMC_VERSION_PATCH   1
 
-#define BMC_VERSION_STR "2.2.0"
+#define BMC_VERSION_STR "2.2.1"
 
 //16 bits unsigned, LSB byte is minor, MSB byte is major
 #define BMC_VERSION ((BMC_VERSION_MAJ<<8) | BMC_VERSION_MIN)
